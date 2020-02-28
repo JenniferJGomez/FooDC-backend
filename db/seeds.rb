c262966654d3b4aaa0175b9d8b56fe2719e99a9f
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-02-28 12:57:19 -0500

    got seed data working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
 require 'rest-client'
 require 'json'
+require 'byebug'
+
+key = Rails.application.credentials.yelp[:api_key]
+
+response_yelp = RestClient.get("https://api.yelp.com/v3/businesses/search?categories=restaurants&location=Washington D.C.", {"Authorization" => "Bearer #{key}"})
+
+response = JSON.parse(response_yelp)
+byebug
+0
