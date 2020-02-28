b7302c36c56262b32b421f391abfd2121ff0fdad
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-02-28 15:14:18 -0500

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,25 @@ require 'rest-client'
 require 'json'
 require 'byebug'
 
+Restaurant.destroy_all
+
 key = Rails.application.credentials.yelp[:api_key]
 
 response_yelp = RestClient.get("https://api.yelp.com/v3/businesses/search?categories=restaurants&location=Washington D.C.", {"Authorization" => "Bearer #{key}"})
 
 response = JSON.parse(response_yelp)
-byebug
-0
+
+response_array = response["businesses"]
+
+response_array.each do |restaurant|
+    name = restaurant["name"]
+    image_url = restaurant["image_url"]
+    url = restaurant["url"]
+    display_phone = restaurant["display_phone"]
+    rating = restaurant["rating"]
+    categories = restaurant["categories"][0]["title"]
+    price = restaurant["price"]
+
+    Restaurant.create(name: name, img_url: image_url, url: url, display_phone: display_phone, rating: rating, category: categories, price: price)
+end
+
