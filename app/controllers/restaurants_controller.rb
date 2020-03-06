2381f9e8ff1a54c19692c5d7497e3945df7eff29
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-03-06 18:17:36 -0500

    worked on reviews routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,12 +11,20 @@ class RestaurantsController < ApplicationController
 
   def show
     restaurant = Restaurant.find(params[:id])
-    render json: restaurant.to_json()
+    render json: restaurant.to_json(
+      :include => {:reviews =>
+        {:except => [:created_at, :updated_at]}}
+    )
   end
 
   def bookmarks
     bookmark = BookmarkedRestaurant.find_or_create_by(user_id: params[:user_id], restaurant_id: params[:restaurant_id])
     render json: bookmark
+  end
+
+  def reviews
+    review = Review.find_or_create_by(user_id: params[:user_id], restaurant_id: params[:restaurant_id], review: params[:review])
+    render json: review
   end
 
   def destroy
